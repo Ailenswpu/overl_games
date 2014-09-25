004200a7724da94dbbb7177fc05443616686ad2c
tuoxiaozhong <281040844@qq.com>
2014-09-25 14:11:35 +0800

    temp

--- app/assets/javascripts/response-footer.js
+++ app/assets/javascripts/response-footer.js
@@ -1,6 +1,7 @@
-$(document).ready(function(){
-  var FooterHeight = $("#footer").height();
-  var WinHeight = $(window).height();
-  var temp = WinHeight-70-FooterHeight;
-  $('#main-window').css("min-height",temp);
+container_height = 0;
+$(window).resize(function() {
+  	var FooterHeight = $("#footer").height();
+	var WinHeight = $(window).height();
+	container_height = WinHeight-70-FooterHeight;
+	$('#main-window').css("min-height",container_height);
 });
