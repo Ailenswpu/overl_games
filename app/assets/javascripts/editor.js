2cb2e6f5f27032be933b49ffb95bac4d34dbbe49
tuoxiaozhong <281040844@qq.com>
2014-09-29 12:10:38 +0800

    add froala

---
+++ app/assets/javascripts/editor.js
@@ -0,0 +1,9 @@
+$(function() {
+        $('#editor').editable({
+        	inlineMode: false,
+        	minHeight: 300,
+        	mediaManager: true,
+        	theme: "dark",
+        	placeholder: "Game Detail"
+        })
+    });
