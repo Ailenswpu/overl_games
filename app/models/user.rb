6a35e2b977222d8d84d01dca536366fd5fe6869e
Ailenswpu <renkaiswpu@gmail.com>
2014-10-05 21:41:11 +0800

    fix bug of user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_many :votes
 
   def self.from_omniauth(auth)
-    where(auth.slice(provider: auth.provider, uid: auth.uid.to_s)).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid.to_s).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
