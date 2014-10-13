ca3205def658baa2bcbc705900f64a310d66d894
tuoxiaozhong <281040844@qq.com>
2014-10-14 00:39:12 +0800

    temp

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,11 @@
 class SubscriptionsController < ApplicationController
   def create
-    if Subscription.create(subscription_params)
+    @subscription = Subscription.new(subscription_params)
+    if @subscription.save
+      flash[:alert] = "恭喜，订阅成功！"
       redirect_to :root
     else
-      flash[:notice] = "请输入正确的邮箱"
+      flash[:notice] = "请输入正确的邮箱!"
       redirect_to :root
     end
   end
